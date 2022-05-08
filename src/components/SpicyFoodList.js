c4d7cfdacef459fb0a39d8e746e576fe9a867af8
Catherine Kawara <ndutakawara2@gmail.com>
2022-05-08 23:35:15 +0300

    add update function

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -6,11 +6,32 @@ function SpicyFoodList() {
 
   function handleAddFood() {
     const newFood = getNewSpicyFood();
+    const newFoodArray = [...foods, newFood]
+    setFoods(newFoodArray)
     console.log(newFood);
   }
 
+  // function handleLiClick(id) {
+  //   const newFoodArray = foods.filter((food) => food.id !== id);
+  //   setFoods(newFoodArray);
+  // }
+
+  function handleLiClick(id) {
+    const newFoodArray = foods.map((food) => {
+      if (food.id === id) {
+        return {
+          ...food,
+          heatLevel: food.heatLevel + 1,
+        };
+      } else {
+        return food;
+      }
+    });
+    setFoods(newFoodArray);
+  }
+
   const foodList = foods.map((food) => (
-    <li key={food.id}>
+    <li onClick={() => handleLiClick(food.id)} key={food.id}>
       {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
     </li>
   ));
